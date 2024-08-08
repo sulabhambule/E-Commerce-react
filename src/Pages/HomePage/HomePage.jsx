421092995775aed664bf36f09a634eb9cd6bee93
Sulabh <sulabhambule@gmail.com>
2024-08-08 18:27:39 +0530

    Done Amazon reat Frontent

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import Cart from "../../Components/Cart/Cart";
+import data from '../../data/product.json'
+
+const HomePage = () => {
+  const [products, setProducts] = useState(data.products);
+
+  return (
+    <div>
+      <div className="products-grid">
+        {products.map((product) => (
+          <Cart key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
